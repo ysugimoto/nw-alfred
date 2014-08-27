003edb9eda6ce8bc8326fad11dde131543376f42
Yoshiaki Sugimoto <sugimoto_yoshiaki@cyberagent.co.jp>
2014-08-27 20:38:58 +0900

    WIP UI/Logic

---
+++ gen.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp index.html node-webkit/node-webkit.app/Contents/Resources/app.nw/index.html
+cp package.json node-webkit/node-webkit.app/Contents/Resources/app.nw/package.json
+cp -R images node-webkit/node-webkit.app/Contents/Resources/app.nw/
